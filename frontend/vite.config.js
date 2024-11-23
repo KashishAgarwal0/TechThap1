1a8a5b7490f5496915f6674329953b1f32d68a15
KashishAgarwal0 <agarwalkashish207@gmail.com>
2024-11-23 19:46:13 +0530

    new1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			input: 'index.html', // Vite looks for index.html as the entry
+		}
+	},
 	server: {
 		port: 3000,
 		// Get rid of the CORS error
